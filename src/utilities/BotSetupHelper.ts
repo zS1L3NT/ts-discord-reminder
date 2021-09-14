22414d02ad50b5c4f23f884ed01027b5c61c7b68
Zechariah Tan <zechariahtan144@gmail.com>
2021-09-14 22:44:53 +0800

    Bot setup doesn't throw error if folder doesn't exist

--- src/utilities/BotSetupHelper.ts
+++ src/utilities/BotSetupHelper.ts
@@ -141,16 +141,21 @@ export default class BotSetupHelper {
 		try {
 			await deployer.deploy()
 		} catch (err) {
-			console.error(
-				`Failed to deploy slash commands for Guild(${guild.name}): ${err.message}`
-			)
+			// @ts-ignore
+			console.error(`Failed to deploy slash commands for Guild(${guild.name}): ${err.message}`)
 		}
 	}
 
 	private setupMessageCommands() {
-		const fileNames = fs
-			.readdirSync(path.join(__dirname, "../messages"))
-			.filter(f => BotSetupHelper.isFile(f))
+		let fileNames: string[]
+
+		try {
+			fileNames = fs
+				.readdirSync(path.join(__dirname, "../messages"))
+				.filter(f => BotSetupHelper.isFile(f))
+		} catch {
+			return
+		}
 
 		for (const messageFileName of fileNames) {
 			const file = require(`../messages/${messageFileName}`) as iMessageFile
@@ -159,8 +164,13 @@ export default class BotSetupHelper {
 	}
 
 	private setupInteractionCommands() {
-		const units = fs
-			.readdirSync(path.join(__dirname, "../commands"))
+		let units: string[]
+
+		try {
+			units = fs.readdirSync(path.join(__dirname, "../commands"))
+		} catch {
+			return
+		}
 
 		// Slash subcommands
 		for (const interactionFolderName of units
@@ -199,9 +209,15 @@ export default class BotSetupHelper {
 	}
 
 	private setupButtonCommands() {
-		const fileNames = fs
-			.readdirSync(path.join(__dirname, "../buttons"))
-			.filter(f => BotSetupHelper.isFile(f))
+		let fileNames: string[]
+
+		try {
+			fileNames = fs
+				.readdirSync(path.join(__dirname, "../buttons"))
+				.filter(f => BotSetupHelper.isFile(f))
+		} catch {
+			return
+		}
 
 		for (const buttonFileName of fileNames) {
 			const buttonFile = require(`../buttons/${buttonFileName}`) as iButtonFile
@@ -210,9 +226,15 @@ export default class BotSetupHelper {
 	}
 
 	private setupMenuCommands() {
-		const fileNames = fs
-			.readdirSync(path.join(__dirname, "../menus"))
-			.filter(f => BotSetupHelper.isFile(f))
+		let fileNames: string[]
+
+		try {
+			fileNames = fs
+				.readdirSync(path.join(__dirname, "../menus"))
+				.filter(f => BotSetupHelper.isFile(f))
+		} catch {
+			return
+		}
 
 		for (const menuFileName of fileNames) {
 			const menuFile = require(`../menus/${menuFileName}`) as iMenuFile
@@ -222,7 +244,6 @@ export default class BotSetupHelper {
 }
 
 export interface iMessageFile {
-	data: string
 	condition: (helper: MessageHelper) => boolean
 	execute: (helper: MessageHelper) => Promise<void>
 }
