7a4db3bdf21e7ba52786929921187c8eefc03335
Zechariah <zechariahtan144@gmail.com>
2022-06-27 20:59:06 +0800

    ✨ move priority changing back to the input box

--- src/interactions/commands/priority.ts
+++ src/interactions/commands/priority.ts
@@ -1,5 +1,4 @@
-import { MessageActionRow, MessageButton, MessageEmbed } from "discord.js"
-import { BaseCommand, CommandHelper } from "nova-bot"
+import { BaseCommand, CommandHelper, CommandType, ResponseBuilder } from "nova-bot"
 
 import Entry from "../../data/Entry"
 import GuildCache from "../../data/GuildCache"
@@ -12,6 +11,28 @@ export default class extends BaseCommand<Entry, GuildCache> {
 		description: "Edit the priority of a Reminder",
 		options: [
 			{
+				name: "priority",
+				description:
+					"Can either be HIGH(7d, 1d, 12h, 2h, 1h, 30m), MEDIUM(1d, 2h) or LOW priority",
+				type: "number" as const,
+				requirements: "Valid Priority",
+				required: true,
+				choices: [
+					{
+						name: "LOW",
+						value: 0
+					},
+					{
+						name: "MEDIUM",
+						value: 1
+					},
+					{
+						name: "HIGH",
+						value: 2
+					}
+				]
+			},
+			{
 				name: "reminder-id",
 				description: "This is the ID of the Reminder to edit",
 				type: "string" as const,
@@ -22,45 +43,55 @@ export default class extends BaseCommand<Entry, GuildCache> {
 		]
 	}
 
+	override only = CommandType.Slash
+
 	override middleware = [new ReminderOrDraftMiddleware()]
 
-	override condition(helper: CommandHelper<Entry, GuildCache>) {
-		return helper.isMessageCommand(null)
-	}
+	override condition(helper: CommandHelper<Entry, GuildCache>) {}
 
-	override converter(helper: CommandHelper<Entry, GuildCache>) {
-		const [reminderId] = helper.args()
-		return {
-			"reminder-id": reminderId
-		}
-	}
+	override converter(helper: CommandHelper<Entry, GuildCache>) {}
 
 	override async execute(helper: CommandHelper<Entry, GuildCache>) {
 		const reminderId = helper.string("reminder-id")
-		const reminder = helper.cache.reminders.find(reminder => reminder.id === reminderId)
-		const draft = helper.cache.draft
+		const index = helper.integer("priority") as 0 | 1 | 2 | null
 
-		helper.respond(
-			{
-				embeds: [
-					new MessageEmbed()
-						.setColor("DARK_GREEN")
-						.setTitle("Edit Priority")
-						.setDescription((reminder || draft)!.getPriorityString())
-						.setFooter({
-							text: reminderId ? "ID: " + reminderId : "Draft"
-						})
-				],
-				components: [
-					new MessageActionRow().addComponents(
-						new MessageButton()
-							.setCustomId("priority")
-							.setLabel("Click to edit")
-							.setStyle("SUCCESS")
-					)
-				]
-			},
-			null
-		)
+		if (index === null) {
+			return helper.respond(
+				ResponseBuilder.bad('Invalid priority, must be either "low", "medium" or "high"')
+			)
+		}
+
+		let oldIndex = -1
+		if (reminderId) {
+			oldIndex = helper.cache.reminders.find(rm => rm.id === reminderId)!.priority
+			await helper.cache.getReminderDoc(reminderId).update({ priority: index })
+		} else {
+			oldIndex = helper.cache.draft!.priority
+			helper.cache.draft!.priority = index
+			await helper.cache.getDraftDoc().update({ priority: index })
+		}
+
+		await helper.respond({
+			embeds: [
+				ResponseBuilder.good(
+					`${reminderId ? "Reminder" : "Draft"} priority updated`
+				).build()
+			],
+			components: []
+		})
+
+		const priorities = ["LOW", "MEDIUM", "HIGH"]
+		helper.cache.logger.log({
+			member: helper.member,
+			title: `Priority Updated`,
+			description: [
+				`<@${helper.member.id}> changed the priority of a Reminder`,
+				`**Reminder ID**: ${reminderId ?? "Draft"}`,
+				`**Old Priority**: ${priorities[oldIndex]}`,
+				`**New Priority**: ${priorities[index]}`
+			].join("\n"),
+			command: "priority",
+			color: "YELLOW"
+		})
 	}
 }
